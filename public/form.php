4b753097258c7bce2ac02d1284d087928270b997
Tom Lecourt-Humbert <tom.lecourt-humbert@etudiant.univ-reims.fr>
2024-09-23 15:26:12 +0200

    Déconnexion de l'utilisateur connecté

--- public/form.php
+++ public/form.php
@@ -8,6 +8,8 @@ use Html\AppWebPage;
 // Création de l'authentification
 $authentication = new UserAuthentication();
 
+$authentication->logoutIfRequested();
+
 $p = new AppWebPage('Authentification');
 
 // Production du formulaire de connexion
@@ -17,11 +19,19 @@ $p->appendCSS(<<<CSS
     }
 CSS
 );
-$form = $authentication->loginForm('auth.php');
-$p->appendContent(<<<HTML
-    {$form}
-    <p>Pour faire un test : essai/toto
-HTML
-);
+if (!$authentication->isUserConnected()) {
+    $form = $authentication->loginForm('auth.php');
+    $p->appendContent(<<<HTML
+        {$form}
+        <p>Pour faire un test : essai/toto
+    HTML
+    );
+} else {
+    $form2 = $authentication->logoutForm('form.php', 'Déconnexion');
+    $p->appendContent(<<<HTML
+        {$form2}
+    HTML
+    );
+}
 
 echo $p->toHTML();
--- src/Authentication/UserAuthentication.php
+++ src/Authentication/UserAuthentication.php
@@ -4,12 +4,18 @@ namespace Authentication;
 
 use Entity\User;
 use Html\StringEscaper;
+use Service\Session;
 
 class UserAuthentication
 {
     use StringEscaper;
     private const LOGIN_INPUT_NAME = 'login';
+    // const == {readOnly}
     private const PASSWORD_INPUT_NAME = 'password';
+    private const SESSION_KEY = '__UserAuthentication__';
+    private const SESSION_USER_KEY = 'user';
+    private const LOGOUT_INPUT_NAME = 'logout';
+    private ?User $user = null;
 
     public function loginForm(string $action, string $submitText = 'OK'): string
     {
@@ -28,6 +34,41 @@ class UserAuthentication
 
     public function getUserFromAuth(): User
     {
-        return User::findByCredentials($_POST['login'], $_POST['password']);
+        $res = User::findByCredentials($_POST['login'], $_POST['password']);
+        $this->setUser($res);
+        return $res;
+    }
+
+    protected function setUser(User $user): void
+    {
+        Session::start();
+        $_SESSION[self::SESSION_KEY] = [];
+        $_SESSION[self::SESSION_KEY][self::SESSION_USER_KEY] = $user;
+        $this->user = $user;
+    }
+
+    public function isUserConnected(): bool
+    {
+        Session::start();
+        return isset($_SESSION[self::SESSION_KEY][self::SESSION_USER_KEY]);
+    }
+
+    public function logoutForm(string $action, string $text): string
+    {
+        $logout = self::LOGOUT_INPUT_NAME;
+        $html = <<<HTML
+        <form action="$action" method="post">
+            <button name="$logout">{$this->escapeString($text)}</button>
+        </form>
+        HTML;
+        return $html;
+    }
+
+    public function logoutIfRequested(): void
+    {
+        Session::start();
+        if (isset($_POST[self::LOGOUT_INPUT_NAME])) {
+            unset($_SESSION[self::SESSION_KEY][self::SESSION_USER_KEY]);
+        }
     }
 }
