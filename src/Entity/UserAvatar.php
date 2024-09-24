bc55504a56d33fce903f725de3fa317b402cf1ca
Tom Lecourt-Humbert <tom.lecourt-humbert@etudiant.univ-reims.fr>
2024-09-24 15:05:31 +0200

    Gestion des données de l'avatar : classe UserAvatar

---
+++ src/Entity/UserAvatar.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Entity;
+
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+
+class UserAvatar
+{
+    private int $id;
+    private ?string $avatar;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getAvatar(): ?string
+    {
+        return $this->avatar;
+    }
+
+    public function findById(int $userId): self
+    {
+        $req = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, avatar
+            FROM user
+            WHERE id = :id
+            SQL
+        );
+        $req->execute(['id' => $userId]);
+        $res = $req->fetchObject(UserAvatar::class);
+        if (false === $res) {
+            throw new EntityNotFoundException('Aucun utilisateur trouvé');
+        }
+
+        return $res;
+    }
+}
